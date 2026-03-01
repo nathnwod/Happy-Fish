166c79901597f50ec4a1a3adc9e162e31a88a1d8
nathan <nathwood@udel.edu>
2026-02-28 23:28:23 -0500

    Completed Nav-Bar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar  from './nav-bar/nav-bar'
+import MainPage from './main-page/main-page'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <MainPage />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/main-page/main-page.jsx
@@ -0,0 +1,11 @@
+import "./main-page.css";
+
+export default function MainPage() {
+  return (
+    <div className="main-page-container">
+        <div className="main-page-content">
+            <h1 className="main-page-title shimmer-wave shimmer-wave-text">Welcome to Happy Fish!</h1>
+        </div>
+    </div>
+  );
+}
